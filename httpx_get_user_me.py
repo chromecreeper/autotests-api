5ec250421104e8c797e8485c4bf461c5807820ad
radionyanya <lmfao@bk.ru>
2025-05-28 18:45:40 +0300

    6.2 httpx update_user task
    also get_user_me var name fixed

--- httpx_get_user_me.py
+++ httpx_get_user_me.py
@@ -13,9 +13,9 @@ login_response_data = login_response.json()
 # Выводим JSON-ответ и статус-код
 print(login_response.status_code)
 
-accessToken = login_response_data["token"]["accessToken"]
+access_token = login_response_data["token"]["accessToken"]
 headers = {
-    "Authorization": f"Bearer {accessToken}"}
+    "Authorization": f"Bearer {access_token}"}
 
 get_user_me_response = httpx.get("http://localhost:8000/api/v1/users/me",headers=headers)
 print(get_user_me_response.status_code)
